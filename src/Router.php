8e91caa494e1d4c0340c4dd4c1ab569f394a34f6
El Boudouti <insafelboudouti@gmail.com>
2023-11-15 16:56:41 +0100

    Début vue

--- src/Router.php
+++ src/Router.php
@@ -1,8 +1,16 @@
 <?php
 
-class Router {
-    public function main() {
-        echo "Hello World";
+require_once("view/view.php");
+class Router 
+{
+    public function main() 
+    {
+        $view = new View();
+        $view->prepareTestPage();
+        $view->render();    
+        $view = new View();
+        $view->prepareAnimalPage("Médor", "chien");
+        $view->render();
     }
 }
 ?>
---
+++ src/view/view.php
@@ -0,0 +1,31 @@
+<?php
+class View 
+{
+    public $title;
+    public $content;
+
+    public function render() 
+    {
+        echo "<!DOCTYPE html>\n";
+        echo "<html>\n";
+        echo "<head>\n";
+        echo "    <title>" . $this->title . "</title>\n";
+        echo "</head>\n";
+        echo "<body>\n";
+        echo "    <h1>" . $this->title . "</h1>\n";
+        echo $this->content;
+        echo "</body>\n";
+        echo "</html>\n";
+    }
+    public function prepareTestPage() 
+    {
+        $this->title = "Page de Test";
+        $this->content = "<p>Ceci est une page de test.</p>";
+    }
+
+    public function prepareAnimalPage($name, $species) {
+        $this->title = "Page sur " . $name;
+        $this->content = "<p>" . $name . " est un animal de l'espèce " . $species . ".</p>";
+    }
+}
+?>
