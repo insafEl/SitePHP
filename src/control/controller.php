d14dd15d6edc53c2b9d9cf9fc7c56d20a8be92be
El Boudouti <insafelboudouti@gmail.com>
2023-11-15 17:26:32 +0100

    Début controlleur

---
+++ src/control/controller.php
@@ -0,0 +1,29 @@
+<?php
+require_once("view/view.php");
+
+class Controller 
+{
+    private $view;
+
+    public function __construct($view) 
+    {
+        $this->view = $view;
+    }
+
+    private $animalsTab = array(
+        'medor' => array('Médor', 'chien'),
+        'felix' => array('Félix', 'chat'),
+        'denver' => array('Denver', 'dinosaure'),
+        // Ajoutez d'autres animaux si vous le souhaitez
+    );
+    
+    public function showInformation($id) {
+        if (array_key_exists($id, $this->animalsTab)) {
+            $animal = $this->animalsTab[$id];
+            $this->view->prepareAnimalPage($animal[0], $animal[1]);
+        } else {
+            $this->view->prepareUnknownAnimalPage();
+        }
+    }
+}
+?>
