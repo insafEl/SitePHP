752c0a6e4112fd7206c17f43dcad39d6bb20f9f8
El Boudouti <insafelboudouti@gmail.com>
2023-11-24 22:14:22 +0100

    css

--- Site.php
+++ Site.php
@@ -15,3 +15,12 @@ $router = new Router();
 $router->main($animalStorage);
 //session_destroy();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Application Title</title>
+    <link rel="stylesheet" href="Style.css"> 
+</html>
